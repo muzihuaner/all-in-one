5c58a6e6c4d1607ac002b8de8c686522cfba2006
muzihuaner <muzihuaner@hotmail.com>
2025-02-09 23:07:52 +0800

    更新

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/allinone.svg',
 
   // Set the production url of your site here
-  url: 'https://allinone.quickso.cn/',
+  url: 'https://allinone.quickso.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
